3e2a4925b7eba0b00a5c224675dbcdfb2a5237d6
Aparna <aparnajay.jay@gmail.com>
2017-07-07 18:54:48 -0700

    Create Book class

---
+++ app/src/main/java/com/example/android/booklisting/Book.java
@@ -0,0 +1,34 @@
+package com.example.android.booklisting;
+
+/**
+ * {@link Book} object contains title and author information on a single book
+ */
+
+public class Book {
+
+    //Book title
+    private String mTitle;
+
+    //Authors
+    private String mAuthors;
+
+    /**
+     * Constructs a new {@link Book} object
+     * @param title is the title of the book
+     * @param authors is/are the author(s) of the book
+     */
+    public Book (String title, String authors){
+        mTitle = title;
+        mAuthors = authors;
+    }
+
+    //Returns title
+    public String getTitle () {
+        return mTitle;
+    }
+
+    //Returns authors
+    public String getmAuthors () {
+        return mAuthors;
+    }
+}
