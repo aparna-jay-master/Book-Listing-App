3bb3a53f1e67df6636c7737134266c27803813f3
Aparna <aparnajay.jay@gmail.com>
2017-07-07 19:40:19 -0700

    Create MainActivity

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,13 +1,116 @@
 package com.example.android.booklisting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<Book>> {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    /** URL for book data from the Google API */
+    private static final String GOOGLE_BOOKS_API =
+            "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=5";
+
+    /**
+     * Constant value for the book loader ID.
+     */
+    private static final int BOOK_LOADER_ID = 1;
+
+    /** Adapter for the list of books */
+    private BookAdapter mAdapter;
+
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView bookListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        bookListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new BookAdapter(this, new ArrayList<Book>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        bookListView.setAdapter(mAdapter);
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(BOOK_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new BookLoader(this, GOOGLE_BOOKS_API);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
+    // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No books found."
+        mEmptyStateTextView.setText(R.string.no_books);
+
+        // Clear the adapter of previous book data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Book}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (books != null && !books.isEmpty()) {
+            mAdapter.addAll(books);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
     }
 }
